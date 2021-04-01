498e6175bced9e6928533e8f610cf1c174b16299
Юрко Ксения <iurko.kk@phystech.edu>
2021-04-01 15:45:23 +0300

    Теперь у меня есть мигалка)))

---
+++ Blinker_RGB.py
@@ -0,0 +1,65 @@
+import pyfirmata
+import time
+
+board = pyfirmata.Arduino("COM4")  
+
+N =[2,3,4,5,6,7]
+
+COLOR1 = str(input('Цвет 1: '))
+COLOR2 = str(input('Цвет 2: '))
+period = float(input('Скорость: '))
+count = int(input('Сколько раз повторить? '))
+
+if COLOR1 == 'red' and COLOR2 == 'blue':
+    for i in range (count):
+        board.digital[5].write(1)
+        time.sleep(period)
+        board.digital[5].write(0)
+        board.digital[4].write(1)
+        time.sleep(period)
+        board.digital[4].write(0)
+    
+elif COLOR1 == 'red' and COLOR2 == 'green':
+    for i in range (count):
+        board.digital[5].write(1)
+        time.sleep(period)
+        board.digital[5].write(0)
+        board.digital[3].write(1)
+        time.sleep(period)
+        board.digital[3].write(0)
+    
+elif COLOR1 == 'blue' and COLOR2 == 'red':
+    for i in range (count):
+        board.digital[7].write(1)
+        time.sleep(period)
+        board.digital[7].write(0)
+        board.digital[2].write(1)
+        time.sleep(period)
+        board.digital[2].write(0)
+    
+elif COLOR1 == 'blue' and COLOR2 == 'green':
+    for i in range (count):
+        board.digital[7].write(1)
+        time.sleep(period)
+        board.digital[7].write(0)
+        board.digital[3].write(1)
+        time.sleep(period)
+        board.digital[3].write(0)
+    
+elif COLOR1 == 'green' and COLOR2 == 'red':
+    for i in range (count):
+        board.digital[6].write(1)
+        time.sleep(period)
+        board.digital[6].write(0)
+        board.digital[2].write(1)
+        time.sleep(period)
+        board.digital[2].write(0)
+
+elif COLOR1 == 'green' and COLOR2 == 'blue':
+    for i in range (count):
+        board.digital[6].write(1)
+        time.sleep(period)
+        board.digital[6].write(0)
+        board.digital[4].write(1)
+        time.sleep(period)
+        board.digital[4].write(0)      
